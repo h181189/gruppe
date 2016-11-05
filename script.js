6ac558ec6906e0978a25f13fb461ea4b3908a117
h181189 <h181189@stud.hib.no>
2016-11-05 22:26:18 +0100

    Optimized webiste and added a profile folder

--- script.js
+++ script.js
@@ -8,13 +8,18 @@ var windowElement;
 var previousWindowWidth;
 
 // Functions
+function isSmallDevice(width) {
+	return width >= 768;
+}
+
 function toggleMenu() {
 	navbarElement.toggle(200);
 }
 
 function toggleOnResize() {
 	var windowWidth = windowElement.width();
-	if (windowWidth >= 992) {
+
+	if (isSmallDevice(windowWidth)) {
 		navbarElement.show();
 	} else {
 		navbarElement.hide();
